46f3c5caa8ad473d1449650f0e2216b19f745908
Vishal <vishal@cybrilla.com>
2018-02-07 18:10:48 +0530

    Initial commit

---
+++ app/src/main/java/com/goleep/driverapp/constants/NetworkConstants.java
@@ -0,0 +1,13 @@
+package com.goleep.driverapp.constants;
+
+/**
+ * Created by vishalm on 07/02/18.
+ */
+
+public class NetworkConstants {
+    private NetworkConstants(){
+
+    }
+
+    public static String BASE_URL = "http://leep.cybrilla.io/";
+}
