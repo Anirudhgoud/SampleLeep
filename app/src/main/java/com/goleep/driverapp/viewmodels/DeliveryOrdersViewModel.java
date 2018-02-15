7275313774211e7d7b67b6e643c2ed287a5d4a43
Anurag Dake <anurag.dake@cybrilla.com>
2018-02-15 23:25:27 +0530

    WIP: Delivery order screen  - Added Activity and fragments with layout files  - Added tabs  - Added required drawables in DO screen for tabs  - Added colors and strings for DO screen  - Added viewmodel for DO activity

---
+++ app/src/main/java/com/goleep/driverapp/viewmodels/DeliveryOrdersViewModel.java
@@ -0,0 +1,17 @@
+package com.goleep.driverapp.viewmodels;
+
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by anurag on 14/02/18.
+ */
+
+public class DeliveryOrdersViewModel extends AndroidViewModel {
+
+    public DeliveryOrdersViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
