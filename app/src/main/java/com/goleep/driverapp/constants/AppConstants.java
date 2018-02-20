6b974d58ed5d12ccb4a00044760377d0b2682a38
Anurag Dake <anurag.dake@cybrilla.com>
2018-02-20 11:49:01 +0530

    Change constant's classes to interfaces

--- app/src/main/java/com/goleep/driverapp/constants/AppConstants.java
+++ app/src/main/java/com/goleep/driverapp/constants/AppConstants.java
@@ -4,9 +4,6 @@ package com.goleep.driverapp.constants;
  * Created by vishalm on 08/02/18.
  */
 
-public class AppConstants {
-    private AppConstants(){
-
-    }
+public interface AppConstants {
 
 }
--- app/src/main/java/com/goleep/driverapp/constants/NetworkConstants.java
+++ app/src/main/java/com/goleep/driverapp/constants/NetworkConstants.java
@@ -4,18 +4,14 @@ package com.goleep.driverapp.constants;
  * Created by vishalm on 07/02/18.
  */
 
-public class NetworkConstants {
-    private NetworkConstants(){
+public interface NetworkConstants {
+    int SUCCESS = 1;
+    int FAILURE = 2;
+    int NETWORK_ERROR = 3;
+    int UNAUTHORIZED = 4;
 
-    }
-
-    public static final int SUCCESS = 1;
-    public static final int FAILURE = 2;
-    public static final int NETWORK_ERROR = 3;
-    public static final int UNAUTHORIZED = 4;
-
-    public static final String PUT_REQUEST = "put";
-    public static final String POST_REQUEST = "post";
-    public static final String GET_REQUEST = "get";
-    public static final String DELETE_REQUEST = "delete";
+    String PUT_REQUEST = "put";
+    String POST_REQUEST = "post";
+    String GET_REQUEST = "get";
+    String DELETE_REQUEST = "delete";
 }
