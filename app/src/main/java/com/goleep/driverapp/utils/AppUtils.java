ef5113741cabc8645372eada65d70546530cc1b5
Anurag Dake <anurag.dake@cybrilla.com>
2018-02-28 13:14:36 +0530

    - Added paint view for accepting signatures - Added util function to get bitmap from view

--- app/src/main/java/com/goleep/driverapp/utils/AppUtils.java
+++ app/src/main/java/com/goleep/driverapp/utils/AppUtils.java
@@ -1,5 +1,9 @@
 package com.goleep.driverapp.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.View;
+
 /**
  * Created by anurag on 19/02/18.
  */
@@ -9,5 +13,13 @@ public class AppUtils {
     public static String userCurrencySymbol(){
         //TODO: Get appropriate currency symbol according to user country or fetch from APIs
         return "₹";
+    }
+
+    public static Bitmap bitmapFromView(View v, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width , height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(0, 0, v.getLayoutParams().width, v.getLayoutParams().height);
+        v.draw(c);
+        return b;
     }
 }
