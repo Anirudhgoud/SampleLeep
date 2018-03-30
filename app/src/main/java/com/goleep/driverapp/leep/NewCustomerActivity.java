0c45df6de51b466dacc5ae676c636b37c3e4d4c9
shubham <shubham@cybrilla.com>
2018-03-30 17:02:36 +0530

    CashSalesNewCustomer ui implemented

---
+++ app/src/main/java/com/goleep/driverapp/leep/NewCustomerActivity.java
@@ -0,0 +1,13 @@
+package com.goleep.driverapp.leep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_customer);
+    }
+}
