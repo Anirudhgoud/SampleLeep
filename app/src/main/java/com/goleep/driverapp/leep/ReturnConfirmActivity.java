52a617101acf3e758bf5c47c04bbd5fec8d5c48a
Vishal <vishal@cybrilla.com>
2018-04-06 16:27:54 +0530

    Resolved conflicts

---
+++ app/src/main/java/com/goleep/driverapp/leep/ReturnConfirmActivity.java
@@ -0,0 +1,13 @@
+package com.goleep.driverapp.leep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReturnConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return_confirm);
+    }
+}
