cdaa5353981e24577f22f580dbb4d1bae80874af
Anurag Dake <anurag.dake@cybrilla.com>
2018-04-10 13:02:32 +0530

    - Added CashSalesPaymentMethodActivity - Added cash_sales_amount_summary_layout for consolidated amounts view

---
+++ app/src/main/java/com/goleep/driverapp/leep/CashSalesPaymentMethodActivity.java
@@ -0,0 +1,24 @@
+package com.goleep.driverapp.leep;
+
+import android.os.Bundle;
+
+import com.goleep.driverapp.R;
+
+public class CashSalesPaymentMethodActivity extends ParentAppCompatActivity {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        setResources(R.layout.activity_cash_sales_payment_method);
+    }
+
+    @Override
+    public void doInitialSetup() {
+
+    }
+
+    @Override
+    public void onClickWithId(int resourceId) {
+
+    }
+
+}
