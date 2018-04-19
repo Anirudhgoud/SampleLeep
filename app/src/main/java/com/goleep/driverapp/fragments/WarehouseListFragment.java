9925670fe0ffafd25844ad6095e2cad6a04a160c
Anurag Dake <anurag.dake@cybrilla.com>
2018-04-19 11:50:17 +0530

    Crash fix in warehouse fragments due to null location

--- app/src/main/java/com/goleep/driverapp/fragments/WarehouseListFragment.java
+++ app/src/main/java/com/goleep/driverapp/fragments/WarehouseListFragment.java
@@ -17,13 +17,10 @@ import com.goleep.driverapp.adapters.WarehouseListAdapter;
 import com.goleep.driverapp.constants.IntentConstants;
 import com.goleep.driverapp.leep.dropoff.dropoff.DropoffActivity;
 import com.goleep.driverapp.leep.dropoff.dropoff.DropoffWarehouseActivity;
+import com.goleep.driverapp.leep.pickup.pickup.PickupActivity;
 import com.goleep.driverapp.leep.pickup.pickup.PickupWarehouseActivity;
 import com.goleep.driverapp.services.room.entities.WarehouseEntity;
-import com.goleep.driverapp.interfaces.UILevelNetworkCallback;
-import com.goleep.driverapp.leep.pickup.pickup.PickupActivity;
 import com.goleep.driverapp.viewmodels.WarehouseViewModel;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,22 +29,18 @@ import butterknife.ButterKnife;
  * Created by vishalm on 23/03/18.
  */
 
-public class WarehouseListFragment extends Fragment{
-    private WarehouseListAdapter adapter;
-    private WarehouseViewModel deliveryOrderViewModel;
+public class WarehouseListFragment extends Fragment {
+
     @BindView(R.id.warehouse_recycler_view)
     RecyclerView recyclerView;
-    private UILevelNetworkCallback deliveryOrdersUILevelCallback = new UILevelNetworkCallback() {
-        @Override
-        public void onResponseReceived(List<?> uiModels, boolean isDialogToBeShown, String errorMessage, boolean toLogout) {
 
-        }
-    };
+    private WarehouseListAdapter adapter;
+    private WarehouseViewModel viewModel;
 
     private View.OnClickListener warehouseSelectionListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-         startPickupActivity(adapter.getItem((Integer) view.getTag()));
+            startPickupActivity(adapter.getItem((Integer) view.getTag()));
         }
     };
 
@@ -62,25 +55,25 @@ public class WarehouseListFragment extends Fragment{
     }
 
     private void initialize() {
-        deliveryOrderViewModel = ViewModelProviders.of(getActivity()).get(WarehouseViewModel.class);
+        viewModel = ViewModelProviders.of(getActivity()).get(WarehouseViewModel.class);
         initRecyclerView();
     }
 
     private void initRecyclerView() {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
-        adapter = new WarehouseListAdapter(deliveryOrderViewModel.getWarehouses());
+        adapter = new WarehouseListAdapter(viewModel.getWarehouses());
         adapter.setWarehouseSelectionListener(warehouseSelectionListener);
         recyclerView.setAdapter(adapter);
     }
 
     private void startPickupActivity(WarehouseEntity warehouseEntity) {
-        if(getActivity() != null && !getActivity().isFinishing()) {
-            if(getActivity().getClass().getSimpleName().equals(PickupWarehouseActivity.class.getSimpleName())) {
+        if (getActivity() != null && !getActivity().isFinishing()) {
+            if (getActivity().getClass().getSimpleName().equals(PickupWarehouseActivity.class.getSimpleName())) {
                 Intent intent = new Intent(getActivity(), PickupActivity.class);
                 intent.putExtra(IntentConstants.WAREHOUSE_ID, warehouseEntity.getId());
                 startActivityForResult(intent, 101);
-            } else if(getActivity().getClass().getSimpleName().equals(DropoffWarehouseActivity.class.getSimpleName())){
+            } else if (getActivity().getClass().getSimpleName().equals(DropoffWarehouseActivity.class.getSimpleName())) {
                 Intent intent = new Intent(getActivity(), DropoffActivity.class);
                 intent.putExtra(IntentConstants.WAREHOUSE_ID, warehouseEntity.getId());
                 startActivityForResult(intent, 101);
