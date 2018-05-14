c2cccea9e9f54b1d4c5111153e5f0def6c9604d2
Anurag Dake <anurag.dake@cybrilla.com>
2018-05-14 20:29:35 +0530

    Add address lines, city and state in new customer api

--- app/src/main/java/com/tracotech/tracoman/leep/dropoff/cashsales/NewCustomerActivity.java
+++ app/src/main/java/com/tracotech/tracoman/leep/dropoff/cashsales/NewCustomerActivity.java
@@ -292,7 +292,8 @@ public class NewCustomerActivity extends ParentAppCompatActivity implements OnMa
                 viewModel.getCustomerInfo().getEmail(), viewModel.getCustomerInfo().getContactName(),
                 viewModel.getCustomerInfo().getContactNumber(), viewModel.getCustomerInfo().getDesignation(),
                 viewModel.getPostalCode(),
-                viewModel.getCustomerInfo().getBusinessTypeId(), viewModel.getCustomerInfo().getCountryDialCode());
+                viewModel.getCustomerInfo().getBusinessTypeId(), viewModel.getCustomerInfo().getCountryDialCode(),
+                etAddressLine1.getText().toString(), etAddressLine2.getText().toString(), etCity.getText().toString(), etState.getText().toString());
     }
 
     private void createNewLocationCall(int businessId) {
